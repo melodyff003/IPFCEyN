e408478fd48656c64b2aadf7c4cf49a5a7fb1e11
Melody Flores <melody03@hotmail.com.ar>
2024-04-21 23:32:15 -0300

    ejercicio 2

--- guia3/ejercicios.hs
+++ guia3/ejercicios.hs
@@ -19,3 +19,22 @@ composicionB b |b == 1 = g(f 1)
                |b == 4 = g(f 4)
                |b == 16 = g(f 16) 
 -------------------------------------------------------
+--Ejercicio 2 a
+absoluto :: Int -> Int
+absoluto x | x >= 0 = x 
+                   | otherwise = -x 
+-- b 
+maximoAbs :: Int -> Int -> Int 
+maximoAbs x y | absoluto x > absoluto y = x
+              | absoluto x < absoluto y = absoluto y
+              |otherwise = -1 
+--c 
+maximo3 :: Int -> Int -> Int -> Int 
+maximo3 a b c | a > b &&  b > c = a
+              | b > a && a > c = b 
+              | c > a && c > b = c
+
+--faltan casos para a > b y b < c 
+maximo2 :: Int -> Int -> Int 
+maximo2 a b | a > b = a
+            |otherwise = b
