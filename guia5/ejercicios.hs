bd8691acf16a63a1dec466265a447aa5d91582a0
Melody Flores <melody03@hotmail.com.ar>
2024-06-16 20:33:38 -0300

    ejercicios guia 5

--- guia5/ejercicios.hs
+++ guia5/ejercicios.hs
@@ -1,7 +1,7 @@
 
 --ej 1
 longitud :: [t] -> Int
-logitud [] = 0 
+longitud [] = 0 
 longitud (x:xs) = 1 + longitud xs
 
 --ej b
@@ -14,11 +14,18 @@ ultimo (x:xs) |longitud (x:xs) == 1 = x
               |longitud (x:xs) > 1 = ultimo xs
 
 
-
 --ej c  
 principio :: [t] -> [t]
 principio (x:xs) | longitud (x:xs) == 2 = x:[]
                  | otherwise = x: principio xs
+--ej d 
+reverso :: [t] -> [t]
+reverso [] = []
+reverso (x:xs) | longitud (x:xs) == 1 = x:[]
+               | otherwise = (reverso xs) ++ (x:[])
+
+lista = [1, 2, 3]
+
 
 --ej 2.a)
 pertenece ::(Eq t) => t-> [t]-> Bool 
@@ -27,4 +34,37 @@ pertenece x (y:ys) | x == y = True
                    | otherwise = pertenece x ys
 
 --ej 2
--- todosIguales :: (Eq t ) => 
+todosIguales :: (Eq t ) => [t] -> Bool 
+todosIguales [] = False
+todosIguales (x:xs) | longitud (x:xs) == 1 = True 
+                    | otherwise = pertenece x xs && todosIguales xs
+-- 2.3
+todosDistintos :: (Eq t) => [t] -> Bool
+todosDistintos [] = True
+todosDistintos (x:xs) | longitud (x:xs) == 1 = True
+                      | otherwise = not(pertenece x xs) && todosDistintos xs 
+
+-- 2.4 
+hayRepetidos :: (Eq t) => [t] -> Bool
+hayRepetidos [] = False
+hayRepetidos (x:xs) | longitud (x:xs) <= 1 = False
+                    | pertenece x xs = True
+                    | otherwise = hayRepetidos xs
+
+--2.5 
+quitar :: (Eq t) => t -> [t] -> [t]
+quitar _ [] = []
+quitar n (x:xs) | n == x = xs -- no hay recursion, entonces solo se ejecuta una vez
+                | otherwise = x:quitar n xs
+
+--2.6 
+quitarTodos :: (Eq t) => t -> [t] -> [t]
+quitarTodos _ [] = []
+quitarTodos n (x:xs) | n == x = quitarTodos n xs
+                     | otherwise = x: quitarTodos n xs 
+
+--2.7 
+eliminarRepetidos :: (Eq t) => [t] -> [t]
+eliminarRepetidos [] = []
+eliminarRepetidos (x:xs) | pertenece x xs = eliminarRepetidos (quitarTodos x xs)
+                         | otherwise = x: eliminarRepetidos xs
